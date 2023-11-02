eb09677b48d1ada9c7cbb676d186c61680ed71af
OkeyIke <benjaminwerner16@gmail.com>
2023-11-02 23:46:20 +0100

    feat(Header.js, SuperHeader.js): add responsive styling and CSS code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants'
+import Logo from '../Logo'
+import SuperHeader from '../SuperHeader'
+import MobileMenu from '../MobileMenu'
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
+import VisuallyHidden from '../VisuallyHidden'
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+  const [showMobileMenu, setShowMobileMenu] = React.useState(false)
 
   // For our mobile hamburger menu, we'll want to use a button
   // with an onClick handler, something like this:
@@ -18,18 +21,32 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </LogoWrapper>
+        <DesktopNav>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
+        </DesktopNav>
+        <MobileActions>
+          <SHoppingBagButton>
+            <VisuallyHidden>Open cart</VisuallyHidden>
+            <Icon id='shopping-bag' />
+          </SHoppingBagButton>
+          <UnstyledButton>
+            <VisuallyHidden>Search</VisuallyHidden>
+            <Icon id='search' />
+          </UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
+            <VisuallyHidden>Open menu</VisuallyHidden>
+            <Icon id='menu' />
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
@@ -37,8 +54,8 @@ const Header = () => {
         onDismiss={() => setShowMobileMenu(false)}
       />
     </header>
-  );
-};
+  )
+}
 
 const MainHeader = styled.div`
   display: flex;
@@ -46,17 +63,60 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-`;
 
-const Nav = styled.nav`
+  @media (${QUERIES.tabletAndSmaller}) {
+    justify-content: space-between;
+    align-items: center;
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
+
+  @media (${QUERIES.phoneAndSmaller}) {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+`
+
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
-`;
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
+`
 
-const Side = styled.div`
+const MobileActions = styled.div`
+  display: none;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media (${QUERIES.phoneAndSmaller}) {
+    gap: 16px;
+  }
+`
+
+const LogoWrapper = styled.div`
   flex: 1;
-`;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    flex: revert;
+  }
+`
+
+const SHoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
+`
+
+const Filler = styled.div`
+  flex: 1;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -68,6 +128,6 @@ const NavLink = styled.a`
   &:first-of-type {
     color: ${COLORS.secondary};
   }
-`;
+`
 
-export default Header;
+export default Header
