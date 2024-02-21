7e6ae8e4509d6eb417945d694b6868c5cf942eaa
OkeyIke <benjaminwerner16@gmail.com>
2024-02-21 10:28:42 +0100

    feat(MobileMenu): Add slide-in animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import styled from 'styled-components/macro'
+import styled, { keyframes } from 'styled-components/macro'
 import { DialogOverlay, DialogContent } from '@reach/dialog'
 
 import UnstyledButton from '../UnstyledButton'
@@ -9,60 +9,105 @@ import VisuallyHidden from '../VisuallyHidden'
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
-    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+    <Wrapper isOpen={isOpen} onDismiss={onDismiss}>
+      <Backdrop />
       <Content aria-label='Menu'>
-        <CloseButton onClick={onDismiss}>
-          <VisuallyHidden>Dismiss menu</VisuallyHidden>
-          <Icon id='close' />
-        </CloseButton>
-        <Filler />
-        <PrimaryNav>
-          <NavLink href='/sale'>Sale</NavLink>
-          <NavLink href='/new'>New&nbsp;Releases</NavLink>
-          <NavLink href='/men'>Men</NavLink>
-          <NavLink href='/women'>Women</NavLink>
-          <NavLink href='/kids'>Kids</NavLink>
-          <NavLink href='/collections'>Collections</NavLink>
-        </PrimaryNav>
-        <FooterNav>
-          <a href='/terms'>Terms and Conditions</a>
-          <a href='/privacy'>Privacy Policy</a>
-          <a href='/contact'>Contact Us</a>
-        </FooterNav>
+        <InnerWrapper>
+          <CloseButton onClick={onDismiss}>
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+            <Icon id='close' />
+          </CloseButton>
+          <Filler />
+          <PrimaryNav>
+            <NavLink href='/sale'>Sale</NavLink>
+            <NavLink href='/new'>New&nbsp;Releases</NavLink>
+            <NavLink href='/men'>Men</NavLink>
+            <NavLink href='/women'>Women</NavLink>
+            <NavLink href='/kids'>Kids</NavLink>
+            <NavLink href='/collections'>Collections</NavLink>
+          </PrimaryNav>
+          <FooterNav>
+            <a href='/terms'>Terms and Conditions</a>
+            <a href='/privacy'>Privacy Policy</a>
+            <a href='/contact'>Contact Us</a>
+          </FooterNav>
+        </InnerWrapper>
       </Content>
-    </Overlay>
+    </Wrapper>
   )
 }
 
-const Overlay = styled(DialogOverlay)`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+
+  to {
+    transform: translateX(0%);
+  }
+`
+
+const Wrapper = styled(DialogOverlay)`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-
-  background: var(--color-backdrop);
-  width: 100dvw;
+  background: transparent;
+  /* width: 100dvw; */
   display: flex;
   justify-content: flex-end;
 `
 
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: var(--color-backdrop);
+  animation: ${fadeIn} 500ms ease-in;
+`
+
 const Content = styled(DialogContent)`
+  --overfill: 16px;
   position: relative;
+  height: 100%;
+  width: calc(300px + var(--overfill));
+  margin-right: calc(var(--overfill) * -1);
+  background: var(--color-white);
+  padding: 24px 16px 32px 32px;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${slideIn} 500ms both cubic-bezier(0, 1.28, 0.95, 1.04);
+    animation-delay: 200ms;
+  }
+`
+
+const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  width: 300px;
-
-  background: var(--color-white);
-  padding: 24px 16px 32px 32px;
+  animation: ${fadeIn} 600ms ease both;
+  animation-delay: 400ms;
 `
 
 const CloseButton = styled(UnstyledButton)`
   position: absolute;
   top: 10px;
-  right: 0;
+  right: var(--overfill);
   padding: 16px;
+  animation: ${slideIn} 200ms ease-in both;
+  animation-delay: 400ms;
 `
 
 const PrimaryNav = styled.nav`
