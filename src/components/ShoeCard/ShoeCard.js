a3a42ed9b866ca3bcc90248c1743e4d2edcfc924
OkeyIke <benjaminwerner16@gmail.com>
2024-02-21 06:06:22 +0100

    feat(ShoeCard): Add CSS animation for image zoom effect

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -35,9 +35,9 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt='' src={imageSrc} />
-          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
-          {variant === 'new-release' && <NewFlag>Just released!</NewFlag>}
         </ImageWrapper>
+        {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+        {variant === 'new-release' && <NewFlag>Just released!</NewFlag>}
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
@@ -68,15 +68,36 @@ const Link = styled.a`
   color: inherit;
 `
 
-const Wrapper = styled.article``
-
-const ImageWrapper = styled.div`
+const Wrapper = styled.article`
   position: relative;
 `
 
-const Image = styled.img`
-  width: 100%;
+const ImageWrapper = styled.div`
   border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
+`
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  transform-origin: 50% 75%;
+  filter: brightness(90%);
+  transition:
+    transform 1000ms,
+    filter 500ms;
+  will-change: transform;
+
+  @media (hover: hover) and (prefers-reduced-motion: no-preference) {
+    ${Link}:hover &, ${Link}:focus & {
+      object-fit: cover;
+      object-position: 50% 30%;
+      transform: scale(1.1);
+      filter: brightness(100%);
+      transition:
+        transform 200ms,
+        filter 400ms;
+    }
+  }
 `
 
 const Row = styled.div`
